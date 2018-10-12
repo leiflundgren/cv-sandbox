041b42a8295eb79b7f621552b7a2467e86fcb2eb
Leif Lundgren <leiflundgren@leiflundgren.com>
2018-10-13 01:13:08 +0200

    rename

---
+++ eye_check.py
@@ -0,0 +1,17 @@
+
+casc_file_frontal = "cascades/haarcascade_frontalface_default.xml"
+casc_file_glasses = 'cascades/haarcascade_eye_tree_eyeglasses.xml'
+
+class EyeCheck:
+    def __init__(self):
+        self.load_cascades()
+        pass
+
+    def load_cascades(self):
+        # Create the haar cascade
+        self.face_frontal_casc = cv2.CascadeClassifier(casc_file_frontal)
+        self.glasses_casc = cv2.CascadeClassifier(casc_file_glasses)
+
+
+    pass
+
--- tests/phototests.py
+++ tests/phototests.py
@@ -1,7 +1,12 @@
 import unittest
+import eye_check
 
 class phototests(unittest.TestCase):
     """description of class"""
+
+    def __init__(self):
+        self.eye_checker = EyeCheck()
+
     pass
 
 if __name__ == '__main__':
