21476da8d7d5205c899422055357958ea163cc3d
Leif Lundgren <l@l>
2018-10-14 17:05:19 +0200

    Detector can detect most spice girls. But not Sporty Spice (Maybe because she leans her head to the side?

--- eye_check.py
+++ eye_check.py
@@ -17,17 +17,40 @@ class EyeChecker:
         trace(6, 'Cascades loaded')
 
     class Check:
-        def __init__(self, im):
+        def __init__(self, parent, imageName, im):
+            trace(8, 'creating Check from ' + imageName)
+            self.parent = parent
+            self.name = imageName
             self.im = im
-        pass
+            self.gray = cv2.cvtColor(self.im, cv2.COLOR_BGR2GRAY)
+            # trace(8, im)
 
-    def check_image(self, image_thing) -> Check:
+            # Detect faces in the image
+            self.faces = self.parent.face_frontal_casc.detectMultiScale(
+                self.gray,
+                scaleFactor=1.12,
+                minNeighbors=8,
+                minSize=(50, 100)
+            )
+            trace(6, 'detecting {0} found {1} faces'.format(self.name, len(self.faces)))
+
+        def show_faces(self):
+            img = self.im.copy()
+            # Draw a rectangle around the faces
+            for (x, y, w, h) in self.faces:
+                cv2.rectangle(img, (x, y), (x+w, y+h), (40, 255, 0), 2)
+                
+
+            cv2.imshow("Faces found", img)
+            cv2.waitKey(0)
+
+    def check_image(self, image_thing) : #-> self.Check:
         
         if isinstance(image_thing, str):
             trace(6, 'loading img ' + image_thing )
             img = cv2.imread(image_thing)
             trace(6, 'loaded img ' + image_thing + ' --> ' + str(type(img)))
-            return Check(img)
+            return self.Check(self, image_thing, img)
         else:
             trace(2, 'cannot load image from ', image_thing)
 
--- test_photos.py
+++ test_photos.py
@@ -1,6 +1,6 @@
 """ Test open-cv detect things in a photo """
-
 import unittest
+
 from eye_check import EyeChecker
 import os
 import common
@@ -12,17 +12,25 @@ class phototests(unittest.TestCase):
 
     def setUp(self):
         common.tracelevel = 9  
-        trace(2, 'phototests running')
+        trace(2, 'setUp: phototests running')
         self.eye_checker = EyeChecker()
         trace(7, 'created eye-check')
 
     def tearDown(self):
-        trace(2,'phototests done')
+        trace(2,'tearDown: phototests done')
 
-    def test_load_image(self):
-        trace(5, 'testing frontal image')
-        m = self.eye_checker.check_image('sample_photos/frontal.jpg')
+    def test_load_spice_girls(self):
+        trace(5, 'testing spice girls image')
+        m = self.eye_checker.check_image('sample_photos/spice-girls.jpg')
         self.assertIsNotNone(m, 'check_image should return possitive')
+        #m.show_faces()
+        self.assertEqual(4, len(m.faces)) # Sporty spice leans head to side?
+
+    def test_load_frontal_glasses(self):
+        file = 'sample_photos/frontal.jpg'
+        trace(5, 'testing ' + file)
+        m = self.eye_checker.check_image(file)
+        m.show_faces()
 
 if __name__ == '__main__':
     unittest.main()
