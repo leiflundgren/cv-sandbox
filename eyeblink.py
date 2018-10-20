967e6fa88cc773ba8f89e869568eb948d6daeb41
Leif Lundgren <l@l>
2018-10-20 17:24:56 +0200

    doc-comment

--- eyeblink.py
+++ eyeblink.py
@@ -13,6 +13,25 @@ import time
 import dlib
 import cv2
 
+# #For dlib’s 68-point facial landmark detector:
+# FACIAL_LANDMARKS_68_IDXS = OrderedDict([
+	# ("mouth", (48, 68)),
+	# ("right_eyebrow", (17, 22)),
+	# ("left_eyebrow", (22, 27)),
+	# ("right_eye", (36, 42)),
+	# ("left_eye", (42, 48)),
+	# ("nose", (27, 36)),
+	# ("jaw", (0, 17))
+# ])
+
+# #For dlib’s 5-point facial landmark detector:
+# FACIAL_LANDMARKS_5_IDXS = OrderedDict([
+	# ("right_eye", (2, 3)),
+	# ("left_eye", (0, 1)),
+	# ("nose", (4))
+# ])
+
+
 if len(sys.argv) <= 1 :
     sys.argv = [sys.argv[0], '-p', 'shape_predictor_68_face_landmarks.dat', '-v', 'camera' ]
 
